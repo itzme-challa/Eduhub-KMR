5e2467835603c7095aaa9c556bc95fc902ca0d60
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-04-22 20:50:04 +0530

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,7 +1,7 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
 
-import { name } from '../../package.json';  // Assuming 'name' is already imported from package.json
+import { name } from '../../package.json';
 
 const debug = createDebug('bot:help_command');
 
@@ -14,6 +14,12 @@ Here are the available commands you can use:\n
 • /neet \\- Access resources for NEET  
 • /jee \\- Access resources for JEE  
 • /study \\- Get study materials for various subjects  
+• /gen \logo name\ \\- Generate a custom logo  
+• /playbio \\| /playphy \\| /playchem \\- Practice random NEET questions  
+• /quote \\- Get a random motivational quote  
+• /mute \\| /unmute \\| /ban \\| /unban \\- Group admin moderation commands  
+• /me \optional\\: @username or userID\ \\- Show user info  
+• /contact \message\ \\- Contact the bot owner directly
 
 For more support or inquiries, you can contact us at:\n
 Email: itzme\\.eduhub\\.contact@gmail\\.com  
@@ -21,7 +27,7 @@ Telegram: [@itzfew](https://t.me/itzfew)`;
 
   debug(`Triggered "help" command with message \n${message}`);
   
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'MarkdownV2' });
+  await ctx.replyWithMarkdownV2(message);
 };
 
 export { help };
