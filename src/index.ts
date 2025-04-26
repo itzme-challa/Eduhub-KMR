e3c4aa0d75a800c7f6062ab9d70e71b815960803
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-04-27 00:13:12 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ import { development, production } from './core';
 import { isPrivateChat } from './utils/groupSettings';
 import { me } from './commands/me'; 
 import { quote } from './commands/quotes';
+import { setupQuizHandlers } from './quizes';
+setupQuizHandlers(bot);
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
